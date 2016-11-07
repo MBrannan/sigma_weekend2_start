0f21e0d4e279b80243de9f222431777d5175319e
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-07 10:32:02 -0600

    I feel like i'm really close

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,38 +1,45 @@
 var cohortMember = 0;
-var i = 0;
 
 $(document).ready(function(){
   $.ajax({
     type: "GET",
     url: "/data",
     success: function(data) {
-      updateDom(data, i);
-      console.log(data);
-      }
-    });
-function updateDom(data, i) {
-    $("#sigma").append().children().last('div class="person"></div>');
-    var $el = $("#sigma").children().last();
-      $el.append('<h2>' + data[i].git_username + '</h2>');
-      $el.append('<p>' + "Username " + data[i].name + '</p>');
-      $el.append('<p>' + "Shoutout " + data[i].shoutout + '</p>');
-      console.log(data.sigmanauts[i].name);
-  }
+      console.log(data.sigmanauts);
+      var students = data.sigmanauts;
+        var currStudent = students[cohortMember];
+        $("#sigma").append().children().last('div class="person"></div>');
+        var $el = $("#sigma").children().last();
+          $el.append('<h2>' + currStudent.git_username + '</h2>');
+          $el.append('<p>' + "Username: " + currStudent.name + '</p>');
+          $el.append('<p>' + "Shoutout: " + currStudent.shoutout + '</p>');
 
-function buttonNext() {
-  $("#nextStudentButton").on("click", function() {
-    cohortMember++;
-    if (cohortMember === sigmanauts.length) {
-      cohortMember = 0;
-    }
-  });
-}
-function buttonPrevious() {
-  $("#previousStudentButton").on("click", function() {
-    cohortMember--;
-    if (cohortMember === 0) {
-      cohortMember = sigmanauts.length;
-    }
-  });
-}
-});
+      $("#nextStudentButton").on("click", function buttonNext() {
+        console.log("testing this");
+          for (var i = 0; i < students.length; i++) {
+              cohortMember++;
+              if (cohortMember === students.length - 1) {
+                cohortMember = 0;
+              }
+            }
+          });
+      $("#previousStudentButton").on("click", function buttonPrevious() {
+        console.log("testing");
+          for (var i = 0; i < students.length; i++) {
+              cohortMember--;
+              if (cohortMember === 0) {
+                cohortMember = students.length;
+              }
+            }
+          });
+        }
+      });
+    });
+// function updateDom(data, i) {
+  //   $("#sigma").append().children().last('div class="person"></div>');
+  //   var $el = $("#sigma").children().last();
+  //     $el.append('<h2>' + data.sigmanauts[i].git_username + '</h2>');
+  //     $el.append('<p>' + "Username " + data.sigmanauts[i].name + '</p>');
+  //     $el.append('<p>' + "Shoutout " + data.sigmanauts[i].shoutout + '</p>');
+  //     console.log(data.sigmanauts[i].name);
+  // }
