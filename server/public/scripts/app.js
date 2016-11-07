ecbacf000c6bc429335becec674e2a110d9f754e
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-07 10:37:58 -0600

    buttons add to display

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,18 +1,19 @@
 var cohortMember = 0;
-
+var students = 0;
 $(document).ready(function(){
   $.ajax({
     type: "GET",
     url: "/data",
     success: function(data) {
       console.log(data.sigmanauts);
-      var students = data.sigmanauts;
-        var currStudent = students[cohortMember];
-        $("#sigma").append().children().last('div class="person"></div>');
-        var $el = $("#sigma").children().last();
-          $el.append('<h2>' + currStudent.git_username + '</h2>');
-          $el.append('<p>' + "Username: " + currStudent.name + '</p>');
-          $el.append('<p>' + "Shoutout: " + currStudent.shoutout + '</p>');
+      students = data.sigmanauts;
+      updateDom();
+        // var currStudent = students[cohortMember];
+        // $("#sigma").append().children().last('div class="person"></div>');
+        // var $el = $("#sigma").children().last();
+        //   $el.append('<h2>' + currStudent.git_username + '</h2>');
+        //   $el.append('<p>' + "Username: " + currStudent.name + '</p>');
+        //   $el.append('<p>' + "Shoutout: " + currStudent.shoutout + '</p>');
 
       $("#nextStudentButton").on("click", function buttonNext() {
         console.log("testing this");
@@ -22,6 +23,7 @@ $(document).ready(function(){
                 cohortMember = 0;
               }
             }
+            updateDom();
           });
       $("#previousStudentButton").on("click", function buttonPrevious() {
         console.log("testing");
@@ -31,15 +33,18 @@ $(document).ready(function(){
                 cohortMember = students.length;
               }
             }
+          updateDom();
           });
         }
       });
     });
-// function updateDom(data, i) {
-  //   $("#sigma").append().children().last('div class="person"></div>');
-  //   var $el = $("#sigma").children().last();
-  //     $el.append('<h2>' + data.sigmanauts[i].git_username + '</h2>');
-  //     $el.append('<p>' + "Username " + data.sigmanauts[i].name + '</p>');
-  //     $el.append('<p>' + "Shoutout " + data.sigmanauts[i].shoutout + '</p>');
-  //     console.log(data.sigmanauts[i].name);
-  // }
+
+    function updateDom(data, i) {
+    var currStudent = students[cohortMember];
+    $("#sigma").append().children().last('div class="person"></div>');
+    var $el = $("#sigma").children().last();
+      $el.append('<h2>' + currStudent.git_username + '</h2>');
+      $el.append('<p>' + "Username " + currStudent.name + '</p>');
+      $el.append('<p>' + "Shoutout " + currStudent.shoutout + '</p>');
+      console.log(currStudent.name);
+  }
